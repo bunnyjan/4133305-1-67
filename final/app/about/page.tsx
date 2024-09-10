abf34e37adc27e80f228fac4b47e985b9fb1942a
bunnyjan <650112418074@bru.ac.th>
2024-09-10 14:54:09 +0700

    final

---
+++ final/app/about/page.tsx
@@ -0,0 +1,19 @@
+import Card from "@/components/about/Card";
+import { FC } from "react";
+
+const About: FC = () =>{
+    return(
+        <>
+            <div>
+            
+            </div>
+            <div className="flex flex-row">
+                <Card/>
+                <Card/>
+            </div>
+        </>
+        
+    )
+}
+
+export default About;
